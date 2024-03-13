4f32f2eab6c6d016577de32aefb4d0f30c109d62
kenniew077 <159483921+kenniew077@users.noreply.github.com>
2024-03-13 15:40:52 -0400

    update
    lab2

--- Lab 2/Rock.gd
+++ Lab 2/Rock.gd
@@ -1,14 +1,29 @@
 extends Sprite2D
-@export var speed=3
-
+@export var speed = 5.
+@export var booleanVariable = true
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	print("tree")
-	pass # Replace with function body.
-
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	if booleanVariable:
+		print("this is true")
+	else:
+		print("this is false")
+	if Input.is_action_pressed("ui_left"):
+		position.x += speed
+	if Input.is_action_pressed("ui_right"):
+		position.x -= speed
+	if Input.is_action_pressed("ui_up"):
+		position.y -= speed
+	if Input.is_action_pressed("ui_down"):
+		position.y += speed
+	#if Input.is_mouse_button_pressed(Mouse_Button_Right):
+		#position.x = position.x + speed.
+		
+	#if speed < 1:
+		hide()
+	#else:
+		show()
 
-	position.x=position.x+speed
-	position.y+=speed
